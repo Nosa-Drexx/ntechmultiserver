8742683005c8a98f3311fd43ff627feceb4c837d
Nosa-Drexx <nosakhare.egharevba@physci.uniben.edu>
2023-02-28 17:22:01 +0100

    cleaned up app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,4 @@
 <script>
-// import MainHeader from './components/MainHeader.vue'
-// import HeroSection from './components/HeroSection.vue'
-// import ServicesSection from './components/ServicesSection.vue'
-// import AboutSection from './components/AboutSection.vue'
-// import ReferenceSection from './components/ReferenceSection.vue'
-// import ContactSection from './components/ContactSection.vue'
-// import FooterSection from './components/FooterSection.vue'
-// import HomeImage from '@/assets/images/home-image.jpg'
 import LandingPage from '@/views/LandingPage/LandingPage.vue'
 export default {
   components: {
